294a33fd471fb0d0648b3c392fe6dbf0329bdc52
Praveen Kumar S <84445957+praveenKumar0007@users.noreply.github.com>
2022-05-09 19:43:34 +0530

    First Commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from openpyxl import Workbook, load_workbook
 
+# To create New sheet
 wb = load_workbook('excel/test_python.xlsx')
-ws = wb.active
-ws['G7'].value = "Loganand"
+ws = wb['Marks']
 
+wb.create_sheet("Test")
 wb.save('excel/test_python.xlsx')
+print(wb.sheetnames)
