4b7e609465e2374e116ba38b05dd2ca026aed01f
Praveen Kumar S <84445957+praveenKumar0007@users.noreply.github.com>
2022-05-09 19:48:23 +0530

    New Workbook

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from openpyxl import Workbook, load_workbook
 
 # To create New sheet
-wb = load_workbook('excel/test_python.xlsx')
-ws = wb['Marks']
+# wb = load_workbook('excel/test_python.xlsx')
+# ws = wb['Marks']
 
-wb.create_sheet("Test")
-wb.save('excel/test_python.xlsx')
-print(wb.sheetnames)
+# wb.create_sheet("Test")
+# wb.save('excel/test_python.xlsx')
+# print(wb.sheetnames)
+
+# To create New Workbook
+
+wb = Workbook()
+ws = wb.active
+ws.title = "Data"
+
+ws.append(["Loganand", "Is", "Great", "!"])
+
+wb.save("excel/Test.xlsx")
