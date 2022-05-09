c8606e22c53ffdda827cedb892a69f965af37be1
Praveen Kumar S <84445957+praveenKumar0007@users.noreply.github.com>
2022-05-09 19:56:29 +0530

    Accessing Multiple Values

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
 
 # To create New sheet
 # wb = load_workbook('excel/test_python.xlsx')
@@ -10,10 +11,22 @@ from openpyxl import Workbook, load_workbook
 
 # To create New Workbook
 
-wb = Workbook()
-ws = wb.active
-ws.title = "Data"
+# wb = Workbook()
+# ws = wb.active
+# ws.title = "Data"
 
-ws.append(["Loganand", "Is", "Great", "!"])
+# ws.append(["Loganand", "Is", "Great", "!"])
+
+# wb.save("excel/Test.xlsx")
+
+# To Access Multiple rows
+
+wb = load_workbook("excel/Test.xlsx")
+ws = wb.active
+
+for row in range(1,11):
+    for col in range(1,5):
+        char = get_column_letter(col)
+        ws[char + str(row)] = char + str(row)
 
 wb.save("excel/Test.xlsx")
